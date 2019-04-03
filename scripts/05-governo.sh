2664084718452e40d9ddf23226b67cfad6bc7462
Marcos Marado <mindboosternoori@gmail.com>
2019-04-03 13:01:15 +0100

    gov: incumprimento resolvido, novo incumprimento

--- scripts/05-governo.sh
+++ scripts/05-governo.sh
@@ -6,7 +6,12 @@
 
 # While we don't have a validator on request, let's find out if a known violation still exists
 ## 20/05/2018: - several images without an alt attribute:
-if [ "$(wget https://www.portugal.gov.pt -o /dev/null -O - | grep "<img" |grep -v alt|wc -l)" -eq "0" ]; then
+## 03/04/2019: - if the alt attribute exists but is empty, it's still an WCAG violation...
+
+fails="$(wget https://www.portugal.gov.pt -o /dev/null -O - | grep "<img" |grep -v alt|wc -l)"
+fails=$((fails + $(wget https://www.portugal.gov.pt -o /dev/null -O - | grep "<img" |grep -c alt=\'\')))
+
+if [ "$fails" -eq "0" ]; then
 	echo "gov.pt: incumprimento pode já não existir";
 else
 	echo "gov.pt: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
