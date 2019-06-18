79f20f85bb694f6e6392c27ef6ee4990555a09c9
Marcos Marado <mindboosternoori@gmail.com>
2019-06-18 18:09:23 +0100

    2020: antigo incumprimento resolvido, mas ha' mais

--- scripts/30-2020.sh
+++ scripts/30-2020.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-naopassa=$(wget https://www.portugal2020.pt/Portal2020/Media/Default/Videos/Login_12_11_2014.htm -O - -o /dev/null|grep EMBED|grep -c swf);
-
-if [ "$naopassa" -eq "1" ]; then
+wget https://www.portugal2020.pt/content/lista-de-operacoes-aprovadas -o /dev/null -O -|hxnormalize -x -l 10000|hxselect .even -c |hxselect .even -c > even
+if [ ! "$(diff even scripts/30/even|wc -l)" -eq "0" ]; then
+	echo "2020: incumprimento pode já não existir";
+else
 	echo "2020: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
 	while IFS='' read -r line || [[ -n "$line" ]]; do
 		test "$(echo "$line"|grep -v -c "2020")" -eq "1" \
@@ -10,6 +11,5 @@ if [ "$naopassa" -eq "1" ]; then
 			|| (h=$(echo "$line"|cut -d\| -f1-4); t=$(echo "$line"|cut -d\| -f6-); echo "$h| $(date +%Y/%m/%d) |$t");
 	done < README.md > new
 	mv new README.md
-else
-	echo "2020: incumprimento resolvido";
 fi
+rm even
