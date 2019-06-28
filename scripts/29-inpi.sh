5c387d3f81829666191dccf26baeffd59ffbd230
Marcos Marado <mindboosternoori@gmail.com>
2019-06-29 00:32:17 +0100

    inpi: precaver indisponibilidade do PDF

--- scripts/29-inpi.sh
+++ scripts/29-inpi.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # guia de utilização que prova que ha' uma dependencia no JAVA
+curl -s --head https://inpi.justica.gov.pt/registos/guia_certificado.pdf | head -n 1 | egrep "HTTP/1.[01] [23]..|HTTP/2 [23].." > /dev/null ; naodisponivel=$?
 wget "https://servicosonline.inpi.pt/registos/guia_certificado.pdf" -o /dev/null
-if [ ! "$(diff guia_certificado.pdf scripts/29/guia_certificado.pdf|wc -l)" -eq "0" ]; then
-	echo "inpi: incumprimento pode já não existir";
+if [ ! "$naodisponivel" -eq "0" ]; then
+	echo "inpi: incumprimento pode já não existir (1)";
+elif [ ! "$(diff guia_certificado.pdf scripts/29/guia_certificado.pdf|wc -l)" -eq "0" ]; then
+	echo "inpi: incumprimento pode já não existir (2)";
 else
 	echo "inpi: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
 	while IFS='' read -r line || [[ -n "$line" ]]; do
