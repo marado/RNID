6537358800dc4ef39d20c09c7cf460c8b1fccce1
Marcos Marado <mindboosternoori@gmail.com>
2019-08-09 12:19:47 +0100

    entrada para o site de eleicoes mais genérica
    Closes: #30

--- scripts/25-eleicoes.sh
+++ scripts/25-eleicoes.sh
@@ -3,7 +3,7 @@
 maybeOK=0;
 
 if [ ! "$(wget https://www.autarquicas2017.mai.gov.pt/ -o -|grep cert|wc -l)" -gt "0" ]; then
-	echo "certificado autarquicas: incumprimento pode já não existir";
+	echo "eleicoes: incumprimento de certificados pode já não existir (mas ha outros erros que ainda nao estao a ser validados)";
 	maybeOK=1;
 fi
 
@@ -15,9 +15,9 @@ fi
 # rm aut
 
 if [ "$maybeOK" -eq "0" ]; then
-	echo "autarquicas: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
+	echo "eleicoes: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
 	while IFS='' read -r line || [[ -n "$line" ]]; do
-		test $(echo "$line"|grep -v autarquicas|wc -l) -eq "1" \
+		test $(echo "$line"|grep -v eleicoes|wc -l) -eq "1" \
 			&& echo "$line" \
 			|| (h=$(echo "$line"|cut -d\| -f1-4); t=$(echo "$line"|cut -d\| -f6-); echo "$h| $(date +%Y/%m/%d) |$t");
 	done < README.md > new
