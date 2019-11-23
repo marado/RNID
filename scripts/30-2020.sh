8a8bcf71383067a7a42b4d443bba996368e16b37
Marcos Marado <mindboosternoori@gmail.com>
2019-11-23 21:44:37 +0000

    portugal2020: grep the entry more explicitly
    Avoid mistakenly update also porto2020's result.

--- scripts/30-2020.sh
+++ scripts/30-2020.sh
@@ -6,7 +6,7 @@ if [ ! "$(diff even scripts/30/even|wc -l)" -eq "0" ]; then
 else
 	echo "2020: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
 	while IFS='' read -r line || [[ -n "$line" ]]; do
-		test "$(echo "$line"|grep -v -c "2020")" -eq "1" \
+		test "$(echo "$line"|grep -v -c "portugal2020")" -eq "1" \
 			&& echo "$line" \
 			|| (h=$(echo "$line"|cut -d\| -f1-4); t=$(echo "$line"|cut -d\| -f6-); echo "$h| $(date +%Y/%m/%d) |$t");
 	done < README.md > new
