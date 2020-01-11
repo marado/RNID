95339371ebc3b6c724386c70b551a3d954537c1d
Marcos Marado <mindboosternoori@gmail.com>
2020-01-11 00:42:49 +0000

    correccao do script para apanhar entrada certa
    Há dois incumprimentos do turismo, actualizar a data só da entrada certa.

--- scripts/35-turismo.sh
+++ scripts/35-turismo.sh
@@ -6,7 +6,7 @@ if [ ! "$(diff page-body scripts/35/page-body|wc -l)" -eq "0" ]; then
 else
 	echo "turismo: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
 	while IFS='' read -r line || [[ -n "$line" ]]; do
-		test "$(echo "$line"|grep -v -c "turismo")" -eq "1" \
+		test "$(echo "$line"|grep -v -c "business")" -eq "1" \
 			&& echo "$line" \
 			|| (h=$(echo "$line"|cut -d\| -f1-4); t=$(echo "$line"|cut -d\| -f6-); echo "$h| $(date +%Y/%m/%d) |$t");
 	done < README.md > new
