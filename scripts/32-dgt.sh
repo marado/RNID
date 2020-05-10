14085971e48dd39a8a8e09086b40e8c84c714fb9
Marcos Marado <mindboosternoori@gmail.com>
2020-05-10 18:28:09 +0100

    incumprimentos mantêm-se, os da dgt foram consolidados

--- scripts/32-dgt.sh
+++ scripts/32-dgt.sh
@@ -2,6 +2,7 @@
 
 naopassa=$(wget http://www.dgterritorio.pt/cartografia_e_geodesia/cartografia/cartografia_de_base___topografica_e_topografica_de_imagem/serie_cartografica_1500_000/ -o /dev/null -O - |html2text|grep atualiz|grep :|cut -d: -f2|grep -c 2017)
 naopassa=$((naopassa + $(wget http://www.dgterritorio.pt/cartografia_e_geodesia/cartografia/carta_administrativa_oficial_de_portugal_caop/caop__download_/carta_administrativa_oficial_de_portugal___versao_2018__em_vigor_/ -o /dev/null -O - |html2text|grep atualiz|grep :|cut -d: -f2|grep -c "janeiro de 2019")))
+# TODO: complementar validações com o incumprimento do XLS
 
 if [ "$naopassa" -ge "1" ]; then
 	echo "dgt: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
@@ -12,5 +13,5 @@ if [ "$naopassa" -ge "1" ]; then
 	done < README.md > new
 	mv new README.md
 else
-	echo "dgt: incumprimento resolvido";
+	echo "dgt: incumprimentos potencialmente resolvidos (verificar cada uma deles manualmente!)";
 fi
