5c3e727dddff3ca7a442da218803698dcc9fc35b
Marcos Marado <mindboosternoori@gmail.com>
2020-08-05 15:11:39 +0100

    DGTerritório: alguns incumprimentos resolvidos
    Dos três incumprimentos apontados, já só encontramos um deles.
    O script foi actualizado para detectar (e só) o incumprimento
    restante, e a tabela de incumprimentos actualizada em conformidade.
    
    Closes: #58

--- scripts/32-dgt.sh
+++ scripts/32-dgt.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-naopassa=$(wget http://www.dgterritorio.pt/cartografia_e_geodesia/cartografia/cartografia_de_base___topografica_e_topografica_de_imagem/serie_cartografica_1500_000/ -o /dev/null -O - |html2text|grep atualiz|grep :|cut -d: -f2|grep -c 2017)
-naopassa=$((naopassa + $(wget http://www.dgterritorio.pt/cartografia_e_geodesia/cartografia/carta_administrativa_oficial_de_portugal_caop/caop__download_/carta_administrativa_oficial_de_portugal___versao_2018__em_vigor_/ -o /dev/null -O - |html2text|grep atualiz|grep :|cut -d: -f2|grep -c "janeiro de 2019")))
+links="$(wget https://tcp.dgterritorio.gov.pt/procurar -o /dev/null -O -|hxnormalize -x -l 10000|hxselect .feed-icon|hxselect a -s '\n')";
+
+# How many links are there? if there is less than 1, something has changed,
+# validate manually. If there are more than 1, maybe there's a XLS but also
+# other (open) format. Let's validate manually and adjust the script if the
+# violation stands.
+nlinks="$(echo "$links"|wc -l)";
+xls="$(echo "$links"|grep -c -i xls)";
+
 # TODO: complementar validações com o incumprimento do XLS
 
-if [ "$naopassa" -ge "1" ]; then
+if [ "$nlinks" -eq "1" ] && [ "$xls" -eq "1" ]; then
 	echo "dgt: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
 	while IFS='' read -r line || [[ -n "$line" ]]; do
 		test "$(echo "$line"|grep -v -c "dgterritorio")" -eq "1" \
@@ -13,5 +20,5 @@ if [ "$naopassa" -ge "1" ]; then
 	done < README.md > new
 	mv new README.md
 else
-	echo "dgt: incumprimentos potencialmente resolvidos (verificar cada uma deles manualmente!)";
+	echo "dgt: algo mudou (nlinks $nlinks, xls $xls), incumprimento potencialmente resolvido (verificar manualmente!)";
 fi
