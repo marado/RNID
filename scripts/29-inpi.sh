2f78b81c0fe10a73a495381ca0f1c8650b17625c
Marcos Marado <mindboosternoori@gmail.com>
2020-12-29 13:49:19 +0000

    inpi: endereço para os serviços mudou
    (mas incumprimento mantém.se)

--- scripts/29-inpi.sh
+++ scripts/29-inpi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # guia de utilização que prova que ha' uma dependencia no JAVA
-curl -s --head https://servicosonline.inpi.pt/registos/guia_certificado.pdf | head -n 1 | egrep "HTTP/1.[01] [23]..|HTTP/2 [23].." > /dev/null ; naodisponivel=$?
-wget "https://servicosonline.inpi.pt/registos/guia_certificado.pdf" -o /dev/null
+curl -s --head https://servicosonline.inpi.justica.gov.pt/registos/guia_certificado.pdf | head -n 1 | egrep "HTTP/1.[01] [23]..|HTTP/2 [23].." > /dev/null ; naodisponivel=$?
+wget "https://servicosonline.inpi.justica.gov.pt/registos/guia_certificado.pdf" -o /dev/null
 if [ ! "$naodisponivel" -eq "0" ]; then
 	echo "inpi: incumprimento pode já não existir (1)";
 elif [ ! "$(diff guia_certificado.pdf scripts/29/guia_certificado.pdf|wc -l)" -eq "0" ]; then
