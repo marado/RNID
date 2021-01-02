d31dcb769d05fb95b0a2b56e964104ee2d0d516a
Marcos Marado <mindboosternoori@gmail.com>
2021-01-02 12:28:35 +0000

    scripts: base: não ficar com cópia do ficheiro

--- scripts/42-base.sh
+++ scripts/42-base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fails="$(wget https://www.base.gov.pt/base2-backoffice/images/fundo_base.png -o -|grep -c ^Unable)"
+fails="$(wget https://www.base.gov.pt/base2-backoffice/images/fundo_base.png -o - -O /dev/null|grep -c ^Unable)"
 
 if [ "$fails" -eq "0" ]; then
 	echo "base: incumprimento pode já não existir (1)";
