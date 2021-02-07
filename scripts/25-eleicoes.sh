7280168f1e3390154b5f5a9792c591ba6a4dcc6a
Marcos Marado <mindboosternoori@gmail.com>
2021-02-07 19:28:13 +0000

    eleicoes: incumprimento mantem-se
    Mas o certificado SSL ficou outra vez mal configurado...

--- scripts/25-eleicoes.sh
+++ scripts/25-eleicoes.sh
@@ -2,7 +2,9 @@
 
 maybeOK=0;
 
-wget "https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fwww.autarquicas2017.mai.gov.pt%2F&profile=css3&usermedium=all&warning=1&vextwarning=&lang=en" -o /dev/null -O - |grep \#errors|cut -d\> -f3|cut -d\< -f1 > aut
+wget "https://jigsaw.w3.org/css-validator/validator?uri=https://www.eleicoes.mai.gov.pt/autarquicas2017/%2F&profile=css3&usermedium=all&warning=1&vextwarning=&lang=en" -o /dev/null -O - |grep \#errors|cut -d\> -f3|cut -d\< -f1 > aut
+# TODO: check https://www.autarquicas2017.mai.gov.pt https certificate
+# wget "https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fwww.autarquicas2017.mai.gov.pt%2F&profile=css3&usermedium=all&warning=1&vextwarning=&lang=en" -o /dev/null -O - |grep \#errors|cut -d\> -f3|cut -d\< -f1 > aut
 if [ ! "$(diff aut scripts/25/aut|wc -l)" -eq "0" ]; then
 	echo "css autarquicas: incumprimento pode já não existir";
 	maybeOK=1;
