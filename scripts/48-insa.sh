b262ebbaf0f9fe766d0361105c5ef8a51ea1cb54
Marcos Marado <mindboosternoori@gmail.com>
2021-04-17 20:20:39 +0100

    Novo incumprimento: INSA, com documentos em XLSX
    Closes: #69

---
+++ scripts/48-insa.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wget http://www.insa.min-saude.pt/category/areas-de-atuacao/epidemiologia/covid-19-curva-epidemica-e-parametros-de-transmissibilidade/ -o /dev/null -O -|hxnormalize -x -l 1000|hxselect tr > INSA
+if [ ! "$(diff INSA scripts/48/INSA|wc -l)" -eq "0" ]; then
+	echo "insa: incumprimento pode já não existir";
+else
+	echo "insa: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
+	while IFS='' read -r line || [[ -n "$line" ]]; do
+		test "$(echo "$line"|grep -v -c "insa")" -eq "1" \
+			&& echo "$line" \
+			|| (h=$(echo "$line"|cut -d\| -f1-4); t=$(echo "$line"|cut -d\| -f6-); echo "$h| $(date +%Y/%m/%d) |$t");
+	done < README.md > new
+	mv new README.md
+fi
+rm INSA
