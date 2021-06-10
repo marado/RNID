639267310a48f27fc8923031a112c14be1171c0b
Marcos Marado <mindboosternoori@gmail.com>
2021-06-10 22:21:03 +0100

    imt: now automated with a test script

---
+++ scripts/49-imt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+fail=0;
+if [ "$(curl https://www.imt-ip.pt/sites/IMTT/Portugues/Formularios/Documents/Mod9IMT.pdf -o /dev/null; echo $?)" -eq "35" ]; then
+	echo "imt: problema com o certificado SSL";
+	fail=1;
+fi
+
+if [ ! "$fail" ]; then
+	echo "imt: incumprimento pode já não existir";
+else
+	echo "imt: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
+	while IFS='' read -r line || [[ -n "$line" ]]; do
+		test $(echo "$line"|grep -v imt|wc -l) -eq "1" \
+			&& echo "$line" \
+			|| (h=$(echo "$line"|cut -d\| -f1-4); t=$(echo "$line"|cut -d\| -f6-); echo "$h| $(date +%Y/%m/%d) |$t");
+	done < README.md > new
+	mv new README.md
+fi
