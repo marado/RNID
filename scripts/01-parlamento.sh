72ca829f9f10c64cd4c9d2968b26f0d3789d44eb
Marcos Marado <mindboosternoori@gmail.com>
2021-09-01 14:08:19 +0100

    parlamento: página mudou ligeiramente
    ...mas o incumprimento mantem-se.

--- scripts/01-parlamento.sh
+++ scripts/01-parlamento.sh
@@ -4,7 +4,7 @@
 
 ## a) Vídeos disponibilizados em WMV
 wget http://www.parlamento.pt/ActividadeParlamentar/Paginas/DetalheAudiencia.aspx?BID=99371 -o /dev/null -O - | \
-	hxnormalize -x -l 10000 | grep ctl00_ctl52_g_1d0614cc_f7c9_4544_a067_a6d1e32c35ae_ctl00_pnlLinksAssociados -A 100 | \
+	hxnormalize -x -l 10000 | grep "Links associados" -A 100 | \
 	grep "Bottom Fixed Nav" -B 100 | hxnormalize -x -l 10000|hxselect a -s '\n'|hxnormalize|grep href|cut -d\" -f2 > tmp
 
 a=$(diff tmp scripts/01/DetalheAudiencia.aspx?BID=99371 |wc -l)
