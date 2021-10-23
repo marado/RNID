bed7df9b0f905b11739c112aa8afe7895aa39268
Marcos Marado <mindboosternoori@gmail.com>
2021-10-23 19:29:12 +0100

    imt: fixing script 50
    The script wasn't actually validating any acessibility issue on IMT's
    website.

--- scripts/50-imt.sh
+++ scripts/50-imt.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# https://www.imt-ip.pt/ - não cumpre WCAG 2.0 AA
+# https://www.imt-ip.pt/ - não cumpre WCAG 2.0 AA (nem A)
 
 #TODO: find a good tool to make the validation on request
 # https://achecker.ca/documentation/web_service_api.php - API usage depends on user account creation
 # webaim.org - API's not gratis
 
-# While we don't have a validator on request, let's find out if a known violation still exists
-## 20/05/2018: - several images without an alt attribute:
-## 28/02/2019: the missing alts case is no longer a problem, but to comply with WCAG the XHTML also needs to be valid...
-##             https://validator.w3.org/nu/?doc=https%3A%2F%2Fwww.iefp.pt%2F
-##             tidy is a great validator, but isn't catching the errors on this
-##             page as errors, even if it shows them as warnings... let's be hacky and pay
-##             attention only on the current use case:
-## 03/04/2019: XHTML is now valid, but there are empty headers now, in violation of levels A and AA
-if [ "$(wget https://www.imt-ip.pt/ -o /dev/null -O -| hxnormalize -x -l 10000|hxselect h2 -s '\n'|grep -c "h2></h2")" -eq "0" ]; then
-	echo "iefp: incumprimento pode já não existir";
+# While we don't have a validator on request, let's find out if a known violation exists
+
+## 23/10/2021: there are empty alts
+
+emptyalt=$(curl -k -L https://www.imt-ip.pt/ | hxclean | hxselect -s '\n' img | hxselect -s '\n' -c 'img::attr(alt)'|grep -c ^$);
+
+if [ "$emptyalt" -eq "0" ]; then
+	echo "imt: incumprimento pode já não existir";
 else
-	echo "iefp: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
+	echo "imt: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
 	while IFS='' read -r line || [[ -n "$line" ]]; do
 		test $(echo "$line"|grep -v imt|wc -l) -eq "1" \
 			&& echo "$line" \
