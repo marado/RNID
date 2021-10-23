d26a380572488e73d49251dc91b979fa982a5ab1
Marcos Marado <mindboosternoori@gmail.com>
2021-10-23 19:37:54 +0100

    Juntando as duas entradas sobre o IMT numa só
    Não vale a pena ter várias entradas para diferentes incumprimentos,
    temos colocado os vários incumprimentos dum site todos juntos.

--- scripts/49-imt.sh
+++ scripts/49-imt.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 fail=0;
+
+# Testing HTTPS
 if [ "$(curl https://www.imt-ip.pt/sites/IMTT/Portugues/Formularios/Documents/Mod9IMT.pdf -o /dev/null; echo $?)" -eq "35" ]; then
 	echo "imt: problema com o certificado SSL";
 	fail=1;
 fi
+
+# Testing HTTPS
 if [ "$(curl https://www.imt-ip.pt/sites/IMTT/Portugues/Formularios/Documents/Mod9IMT.pdf -o /dev/null; echo $?)" -eq "60" ]; then
 	echo "imt: problema com o certificado SSL";
 	fail=2;
+fi
+
+# https://www.imt-ip.pt/ - não cumpre WCAG 2.0 AA (nem A)
+## 23/10/2021: there are empty alts
+emptyalt=$(curl -k -L https://www.imt-ip.pt/ | hxclean | hxselect -s '\n' img | hxselect -s '\n' -c 'img::attr(alt)'|grep -c ^$);
+if [ "$emptyalt" -ne "0" ]; then
+	echo "imt: problemas de acessibilidade";
+	fail=3;
 fi
 
 if [ "$fail" = 0 ]; then
