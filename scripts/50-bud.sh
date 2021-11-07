3217d6d3ad41fc799206f240454fb763a088266e
Marcos Marado <mindboosternoori@gmail.com>
2021-11-07 00:43:46 +0000

    bud: novo incumprimento
    O site https://bud.gov.pt/ddn/dispensa/requerer.html tem um documento
    de requerimento exclusivamente em formato doc, em incumprimento com a
    Lei das Normas Abertas.
    
    Closes: #78

---
+++ scripts/50-bud.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+files=$(
+	for i in $(wget https://bud.gov.pt/ddn/dispensa/requerer.html -o /dev/null -O -); do echo $i; done | \
+		grep href|cut -d\" -f2|grep -v html$|grep -v ^#|grep -v ^http|grep -v "\.css"|grep -v png$|grep -v ico$
+);
+nfiles=$(echo "$files"|wc -l);
+docs=$(echo "$files"|grep .doc$|wc -l);
+
+if [ "$nfiles" != 1 ] || [ "$docs" != 1 ]; then
+	echo "bud: incumprimento pode já não existir";
+else
+	echo "bud: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
+	while IFS='' read -r line || [[ -n "$line" ]]; do
+		test $(echo "$line"|grep -v bud|wc -l) -eq "1" \
+			&& echo "$line" \
+			|| (h=$(echo "$line"|cut -d\| -f1-4); t=$(echo "$line"|cut -d\| -f6-); echo "$h| $(date +%Y/%m/%d) |$t");
+	done < README.md > new
+	mv new README.md
+fi
