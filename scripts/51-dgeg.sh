1d5f831b13128567cb0f99dea4f4312d71f92948
Marcos Marado <mindboosternoori@gmail.com>
2022-03-25 16:35:28 +0000

    novo incumprimento: Dir. Geral de Energia e Geo.
    A informação sobre vendas mensais de derivados de petróleo (pelo
    menos) encontra-se apenas em formato XLSX, em violação ao Regulamento
    Nacional de Interoperabilidade Digital.
    
    Closes: #91

---
+++ scripts/51-dgeg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wget https://www.dgeg.gov.pt/pt/estatistica/energia/petroleo-e-derivados/vendas-mensais/ -o /dev/null -O dgeg
+
+if [ ! "$(diff dgeg scripts/51/dgeg|wc -l)" -eq "0" ]; then
+	echo "dgeg: incumprimento pode já não existir";
+else
+	echo "dgeg: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
+	while IFS='' read -r line || [[ -n "$line" ]]; do
+		test "$(echo "$line"|grep -v -c "dgeg")" -eq "1" \
+			&& echo "$line" \
+			|| (h=$(echo "$line"|cut -d\| -f1-4); t=$(echo "$line"|cut -d\| -f6-); echo "$h| $(date +%Y/%m/%d) |$t");
+	done < README.md > new
+	mv new README.md
+fi
+rm dgeg
