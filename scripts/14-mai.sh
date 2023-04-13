419e543a19c46441f0b99c88dfa52515f19cc546
Marcos Marado <mindboosternoori@gmail.com>
2023-04-13 16:42:18 +0100

    14-mai.sh: adicionado DEBUG
    Quando o incumprimento parece já não existir, passamos a enviar para o
    output o conteúdo do que foi obtido, para que tal possa ser validado
    sem precisar de correr o script outra vez.
    
    Ref.: #102

--- scripts/14-mai.sh
+++ scripts/14-mai.sh
@@ -2,9 +2,11 @@
 
 wget --no-check-certificate https://www.sg.mai.gov.pt/AdministracaoEleitoral/RecenseamentoEleitoral/ResultadosRecenseamento/Paginas/default.aspx -o /dev/null -O -|grep -v __REQUESTDIGEST|grep -v VIEWSTATE|hxnormalize -x -l 1000|hxselect .conteudo > mai-tmp
 cat mai-tmp|hxselect a -s'\n' > mai
-rm mai-tmp
 if [ ! "$(diff mai scripts/14/mai|wc -l)" -eq "0" ]; then
 	echo "mai: incumprimento pode já não existir";
+	echo "DEBUG: mai-tmp:"
+	cat mai-tmp
+	echo "EOF"
 else
 	echo "mai: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
 	while IFS='' read -r line || [[ -n "$line" ]]; do
@@ -14,4 +16,4 @@ else
 	done < README.md > new
 	mv new README.md
 fi
-rm mai
+rm mai-tmp mai
