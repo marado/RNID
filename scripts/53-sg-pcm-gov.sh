e52da78a899e09e7450ba5147722ecd4f428dbcc
Marcos Marado <mindboosternoori@gmail.com>
2024-10-19 16:04:00 +0100

    script: sg.pcm: NOTA mudada para a secção certa
    É nos casos onde não obtemos URL algum que pode ser porque nem sequer
    obtivemos a página...

--- scripts/53-sg-pcm-gov.sh
+++ scripts/53-sg-pcm-gov.sh
@@ -7,11 +7,11 @@ ndocx=$(echo "$urls" | grep -v '^$' |grep -c -v docx);
 
 if [ "$nurls" -eq "0" ]; then
 	echo "sg.pcm.gov.pt: não foram encontrados urls, script não deve estar a funcionar correctamente.";
+	echo "NOTA: tentaste isto vindo dos IPs do github? Se sim, provavelmente bateste num 405 Method Not Allowed...";
 	echo "DEBUG:"
 	wget https://www.sg.pcm.gov.pt/servicos/fundacoes-e-entidades-de-utilidade-publica/ && cat index.html && rm index.html
 elif ! [ "$ndocx/$nurls" = "4/8" ]; then
 	echo "sg.pcm.gov.pt: existiam 4/8 endereços em .docx, e agora são ($ndocx/$nurls). Verificar se incumprimento se mantem";
-	echo "NOTA: tentaste isto vindo dos IPs do github? Se sim, provavelmente bateste num 405 Method Not Allowed...";
 else
 	echo "sg.pcm.gov.pt: Incumprimento mantém-se, há ($ndocx/$nurls) endereços em .docx, a actualizar o README (faça um git diff, valide, e commit!)";
 	while IFS='' read -r line || [[ -n "$line" ]]; do
