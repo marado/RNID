5cbc97b49b0e6d72c53f18fb00dd38f9b0c740d7
Marcos Marado <mindboosternoori@gmail.com>
2025-03-29 17:48:12 +0000

    novo incumprimento: anqep - catálogo nacional de qualificações
    Falta: pedir resolução do incumprimento
    PS: o github é um penico!
    
    Closes: #106

---
+++ scripts/55-anqep.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ufcdPesquisa (HTML) includes a few javascripts, including two main-es*5.*js , which are the ones that show that we're getting XLS results
+# As long as those two imports remain the same, so does the RNID violation...
+wget https://catalogo.anqep.gov.pt/ufcdPesquisa -o /dev/null
+
+if [ ! "$(diff ufcdPesquisa scripts/55/ufcdPesquisa|wc -l)" -eq "0" ]; then
+	echo "anqep: incumprimento pode já não existir";
+else
+	echo "anqep: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
+	while IFS='' read -r line || [[ -n "$line" ]]; do
+		test "$(echo "$line"|grep -v -c "anqep")" -eq "1" \
+			&& echo "$line" \
+			|| (h=$(echo "$line"|cut -d\| -f1-4); t=$(echo "$line"|cut -d\| -f6-); nc=$(echo "$line"|cut -d\| -f5 | wc -m); printf "%s| %-$((nc-2))s|%s\n" "$h" "$(date +%Y/%m/%d)" "$t");
+	done < README.md > new
+	mv new README.md
+fi
