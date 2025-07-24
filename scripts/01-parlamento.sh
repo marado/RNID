32eea31eb19cf6abf307ed14d3858131686b0154
Marcos Marado <mindboosternoori@gmail.com>
2025-07-24 20:06:55 +0100

    parlamento: simplificação (e melhoria) do script

--- scripts/01-parlamento.sh
+++ scripts/01-parlamento.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
 
-# Incumprimentos: Vídeos disponibilizados em WMV, Canal Parlamento em Flash
-
-## a) Vídeos disponibilizados em WMV
+## Vídeos disponibilizados em WMV
 wget 'http://www.parlamento.pt/ActividadeParlamentar/Paginas/DetalheAudiencia.aspx?BID=99371' -o /dev/null -O - | \
 	hxnormalize -x -l 10000 2>/dev/null | grep "Links associados" -A 100 | \
 	grep "Bottom Fixed Nav" -B 100 | hxnormalize -x -l 10000|hxselect a -s '\n'|hxnormalize|grep href|cut -d\" -f2 > tmp
 
 a=$(diff tmp scripts/01/DetalheAudiencia.aspx?BID=99371 |wc -l)
-# Se $a for 0, então o incumprimento mantém-se
 rm tmp
 
-## b) Canal Parlamento em Flash
-b=0; # 0 significa que incumprimento mantém-se
-test $(wget --no-check-certificate https://canal.parlamento.pt/ -o /dev/null -O -|grep embedplayer.min.js|wc -l) -eq "1" \
-	&& (
-		wget --no-check-certificate https://canal.parlamento.pt/scripts/embedplayer.min.js -o /dev/null;
-		test "$(diff embedplayer.min.js scripts/01/embedplayer.min.js|wc -l)" -eq "0" || b=1;
-		rm embedplayer.min.js;
-	) || b=2
-
 ## resultados:
+# Se $a for 0, então o incumprimento mantém-se
 if [ ! "$a" -eq "0" ]; then
-	echo "parlamento: Incumprimento 'a' (videos em wmv) pode estar resolvido.";
-fi
-if [ ! "$b" -eq "0" ]; then
-	echo "parlamento: Incumprimento 'b' (flash no canal paralamento) pode estar resolvido ($b).";
-fi
-if [ $((a + b)) -eq "0" ]; then
 	echo "parlamento: incumprimento mantém-se, a actualizar README (faça um git diff, valide, e commit!)";
 	while IFS='' read -r line || [[ -n "$line" ]]; do
 		test $(echo "$line"|grep -v www.parlamento.pt|wc -l) -eq "1" \
@@ -36,9 +19,5 @@ if [ $((a + b)) -eq "0" ]; then
 	done < README.md > new
 	mv new README.md
 else
-	# TODO: fazer disto o teste (c) (e meter este incumprimento também no README)
-    ## c) certificado mal configurado
-    ##   -- https://whatsmychaincert.com/?canal.parlamento.pt
-    ##   -- TODO: escrever o teste!
-	echo "https://www.canal.parlamento.pt/ ainda tem problemas de https?";
+	echo "parlamento: incumprimento pode já não existir";
 fi
