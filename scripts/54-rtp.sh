c0545efa94ec013165236c91cf72fb5b5fc7c7a4
Marcos Marado <mindboosternoori@gmail.com>
2025-08-31 19:12:16 +0100

    Revert "RTP: incumprimento resolvido!"
    This reverts commit e08b1a8f3a97b93bc35d6a61dc91a264015e3036.
    
    Além da reversão simples, o script e o texto no README foram
    alterados, porque agora o DRM não se verifica em directo, mas continua
    presente em conteúdo em diferido.

---
+++ scripts/54-rtp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+url="https://www.rtp.pt/play/direto/rtp1"
+url="https://www.rtp.pt/play/p15472/e872635/a-juventude-de-jane"
+
+if [ "$(wget $url -o /dev/null -O - |grep -c 'drm: true')" -ne "1" ]; then
+	echo "RTP: não encontrei DRM no stream testado, incumprimento pode já não existir";
+else
+	echo "RTP: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
+	while IFS='' read -r line || [[ -n "$line" ]]; do
+		test "$(echo "$line"|grep -v -c "rtp")" -eq "1" \
+			&& echo "$line" \
+			|| (h=$(echo "$line"|cut -d\| -f1-4); t=$(echo "$line"|cut -d\| -f6-); nc=$(echo "$line"|cut -d\| -f5 | wc -m); printf "%s| %-$((nc-2))s|%s\n" "$h" "$(date +%Y/%m/%d)" "$t");
+	done < README.md > new
+	mv new README.md
+fi
