74a8202d384a6632d3abfef9b8782c6660b6d120
Marcos Marado <mindboosternoori@gmail.com>
2026-01-28 14:40:02 +0000

    snirh: script actualizado
    A APA gosta de brincar aos gatos e aos ratos, em vez de perder tempo a
    cumprir a lei, perde tempo a impedir que o site seja testado...
    
    Pois bem, agora em vez de fazer uma validação automatica ao site todo,
    o script descarrega a página e testa um dos 107 erros de HTML
    detetados. Quando esse estiver resolvido, teremos de fazer uma nova
    validação manual...

--- scripts/26-snirh.sh
+++ scripts/26-snirh.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 # | https://snirh.apambiente.pt/ | XHTML inválido, CSS inválido | [análise do XHTML](https://validator.w3.org/check?uri=https%3A%2F%2Fsnirh.apambiente.pt&charset=%28detect+automatically%29&doctype=Inline&group=0), [análise do CSS](https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fsnirh.apambiente.pt&profile=css3&usermedium=all&warning=1&vextwarning=&lang=en) | 2018/06/01 ||
+#
+# We used to have the HTML and CSS tested automagically with w3 validators, but they were blocked by APA.
+# Very well, we'll test those errors manually...
+#
+# At 28/01/2026 there were 107 HTML errors detected on the front page.
+# We're testing just the first of them, to see if it still exists:
+#
+# We have a line like this:
+# <link href="/_classes/css/gp-buttons/css/css3-buttons.css" rel="stylesheet" type="text/css" media="screen">
+# which is an error: end tag for "link" omitted, but OMITTAG NO was specified
+# The solution should be: perhaps you meant to "self-close" an element, that is, ending it with "/>" instead of ">".
+# Let's see if the issue is still there:
+xhtmlErrors=$(wget "https://snirh.apambiente.pt/" -o /dev/null -O -|grep media=\"screen|grep -v /\>|wc -l);
 
-maybeOK=0;
-
-# Test disabled as the website seems to be 403ing the request
-# xhtmlErrors=$(wget "https://validator.w3.org/check?uri=https%3A%2F%2Fsnirh.apambiente.pt&charset=%28detect+automatically%29&doctype=Inline&group=0" -o /dev/null -O - |hxnormalize -x -l 1000|hxselect .invalid|hxselect h3 -c|cut -d: -f2|cut -d" " -f2)
-# if [ "$xhtmlErrors" -eq "0" ]; then
-# 	echo "xhtml snirh: incumprimento já não existe";
-# 	maybeOK=1;
-# fi
-
-wget "https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fsnirh.apambiente.pt&profile=css3&usermedium=all&warning=1&vextwarning=&lang=en" -o /dev/null -O - |grep \#errors|cut -d\> -f3|cut -d\< -f1 > css
-if [ ! "$(diff css scripts/26/css|wc -l)" -eq "0" ]; then
-	echo "css snirh: incumprimento pode já não existir";
-	maybeOK=1;
-fi
-rm css
-
-if [ "$maybeOK" -eq "0" ]; then
+if [ "$xhtmlErrors" -eq "0" ]; then
+	echo "snirh: detalhe do incumprimento que foi testado já não existe, incumprimento pode estar resolvido (testar manualmente)";
+else
 	echo "snirh: Incumprimento mantém-se, a actualizar o README (faça um git diff, valide, e commit!)";
 	while IFS='' read -r line || [[ -n "$line" ]]; do
 		test $(echo "$line"|grep -v snirh|wc -l) -eq "1" \
